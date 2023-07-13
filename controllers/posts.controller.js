5f758e73b81bf9e8762811a51fe47a89fea39ac6
choisumin <tnals634@naver.com>
2023-07-13 12:04:25 +0900

    fix: posts refactoring
    posts의 controller, services 수정

--- controllers/posts.controller.js
+++ controllers/posts.controller.js
@@ -1,8 +1,7 @@
 const PostService = require('../services/posts.services');
-const UserService = require('../services/users.services');
+
 class PostsController {
   postService = new PostService();
-  userService = new UserService();
 
   getPosts = async (req, res, next) => {
     const allPosts = await this.postService.findAllPost();
@@ -14,20 +13,11 @@ class PostsController {
     const { post_id } = req.params;
 
     try {
-      await this.postService.findOnePost(post_id);
-    } catch (error) {
-      return res
-        .status(404)
-        .json({ errorMessage: '게시글이 존재하지 않습니다.' });
-    }
-    try {
       const post = await this.postService.findOnePost(post_id);
 
       res.status(200).json({ post: post });
     } catch (error) {
-      return res
-        .status(500)
-        .json({ errorMessage: '게시글 상세 조회에 실패하였습니다.' });
+      return res.status(error.status).json({ errorMessage: error.message });
     }
   };
 
@@ -36,33 +26,10 @@ class PostsController {
     const { user_id } = res.locals.user;
 
     try {
-      const user = await this.userService.findUserById(user_id);
-      if (!title || !content) {
-        return res
-          .status(400)
-          .json({ errorMessage: '데이터 형식이 올바르지 않습니다.' });
-      } else if (typeof title != 'string') {
-        return res
-          .status(400)
-          .json({ errorMessage: '게시글 제목의 형식이 일치하지 않습니다.' });
-      } else if (typeof content != 'string') {
-        return res
-          .status(400)
-          .json({ errorMessage: '게시글 내용의 형식이 일치하지 않습니다.' });
-      }
-
-      await this.postService.createPost(
-        user.user_id,
-        user.nickname,
-        title,
-        content,
-        0
-      );
+      await this.postService.createPost(user_id, title, content, 0);
       res.status(201).json({ message: '게시글 작성에 성공하였습니다.' });
     } catch (error) {
-      return res
-        .status(500)
-        .json({ errorMessage: '게시글 작성에 실패하였습니다.' });
+      return res.status(error.status).json({ errorMessage: error.message });
     }
   };
 
@@ -72,46 +39,11 @@ class PostsController {
     const { user_id } = res.locals.user;
 
     try {
-      await this.postService.findOnePost(post_id);
-    } catch (error) {
-      return res
-        .status(404)
-        .json({ errorMessage: '게시글이 존재하지 않습니다.' });
-    }
-
-    try {
-      const post = await this.postService.findOnePost(post_id);
-      if (!title || !content) {
-        return res
-          .status(400)
-          .json({ errorMessage: '데이터 형식이 올바르지 않습니다.' });
-      } else if (user_id != post.User_id) {
-        return res
-          .status(403)
-          .json({ errorMessage: '게시글 수정 권한이 존재하지 않습니다.' });
-      } else if (typeof title != 'string') {
-        return res
-          .status(400)
-          .json({ errorMessage: '게시글 제목의 형식이 일치하지 않습니다.' });
-      } else if (typeof content != 'string') {
-        return res
-          .status(400)
-          .json({ errorMessage: '게시글 내용의 형식이 일치하지 않습니다.' });
-      }
-
-      await this.postService.updatePost(
-        user_id,
-        post_id,
-        title,
-        content,
-        post.likes
-      );
+      await this.postService.updatePost(user_id, post_id, title, content);
 
       res.status(200).json({ message: '게시글을 수정하였습니다.' });
     } catch (error) {
-      return res
-        .status(500)
-        .json({ errorMessage: '게시글 수정에 실패하였습니다.' });
+      return res.status(error.status).json({ errorMessage: error.message });
     }
   };
 
@@ -120,28 +52,11 @@ class PostsController {
     const { user_id } = res.locals.user;
 
     try {
-      await this.postService.findOnePost(post_id);
-    } catch (error) {
-      return res
-        .status(404)
-        .json({ errorMessage: '게시글이 존재하지 않습니다.' });
-    }
-
-    try {
-      const post = await this.postService.findOnePost(post_id);
-      if (user_id != post.User_id) {
-        return res
-          .status(403)
-          .json({ errorMessage: '게시글 삭제 권한이 존재하지 않습니다.' });
-      }
-
-      await this.postService.deletePost(post_id, post.User_id);
+      await this.postService.deletePost(post_id, user_id);
 
       res.status(200).json({ message: '게시글이 삭제되었습니다.' });
     } catch (error) {
-      return res
-        .status(500)
-        .json({ errorMessage: '게시글 삭제에 실패하였습니다.' });
+      return res.status(error.status).json({ errorMessage: error.message });
     }
   };
 
@@ -150,50 +65,10 @@ class PostsController {
     const { user_id } = res.locals.user;
 
     try {
-      await this.postService.findOnePost(post_id);
+      const update = await this.postService.createLike(post_id, user_id);
+      return res.status(update.updateStatus).json({ message: update.message });
     } catch (error) {
-      return res
-        .status(404)
-        .json({ errorMessage: '게시글이 존재하지 않습니다.' });
-    }
-
-    try {
-      await this.postService.findOneLike(post_id, user_id);
-    } catch (error) {
-      await this.postService.createLike(post_id, user_id);
-      const post = await this.postService.findOnePost(post_id);
-      const findAllLike = await this.postService.findAllLike(post_id);
-
-      await this.postService.updatePost(
-        post.User_id,
-        post.post_id,
-        post.title,
-        post.content,
-        findAllLike.length
-      );
-      return res
-        .status(200)
-        .json({ message: `${post.title}의 좋아요를 등록하였습니다.` });
-    }
-    try {
-      const post = await this.postService.findOnePost(post_id);
-      await this.postService.deleteLike(post_id, user_id);
-      const findAllLike = await this.postService.findAllLike(post_id);
-
-      await this.postService.updatePost(
-        post.User_id,
-        post.post_id,
-        post.title,
-        post.content,
-        findAllLike.length
-      );
-      res
-        .status(200)
-        .json({ message: `${post.title}의 좋아요를 취소하였습니다.` });
-    } catch (like) {
-      return res
-        .status(500)
-        .json({ errorMessage: '좋아요를 수정할 수 없습니다.' });
+      return res.status(error).json({ errorMessage: error.message });
     }
   };
 
--- services/posts.services.js
+++ services/posts.services.js
@@ -1,11 +1,28 @@
 const PostRepository = require('../repositories/posts.repository');
+const UserRepository = require('../repositories/users.repository');
+
 class PostService {
   postRepository = new PostRepository();
+  userRepository = new UserRepository();
 
-  createPost = async (user_id, nickname, title, content, likes) => {
+  createPost = async (user_id, title, content, likes) => {
+    const user = await this.userRepository.findUserById(user_id);
+    const error = new Error();
+    if (!title || !content) {
+      error.message = '데이터 형식이 올바르지 않습니다.';
+      error.status = 400;
+      throw error;
+    } else if (typeof title != 'string') {
+      error.message = '게시글 제목의 형식이 일치하지 않습니다.';
+      error.status = 400;
+      throw error;
+    } else if (typeof content != 'string') {
+      error.message = '게시글 내용의 형식이 일치하지 않습니다.';
+      error.status = 400;
+    }
     const post = await this.postRepository.createPost(
-      user_id,
-      nickname,
+      user.user_id,
+      user.nickname,
       title,
       content,
       likes
@@ -43,7 +60,12 @@ class PostService {
 
   findOnePost = async (post_id) => {
     const post = await this.postRepository.findOnePost(post_id);
-
+    const error = new Error();
+    if (!post) {
+      error.message = '게시글이 존재하지 않습니다.';
+      error.status = 404;
+      throw error;
+    }
     return {
       post_id: post.post_id,
       User_id: post.User_id,
@@ -56,16 +78,36 @@ class PostService {
     };
   };
 
-  updatePost = async (user_id, post_id, title, content, likes) => {
-    const findPost = await this.postRepository.findOnePost(post_id);
-    if (!findPost) throw new Error("Post doesn't exist");
-
+  updatePost = async (user_id, post_id, title, content) => {
+    const post = await this.postRepository.findOnePost(post_id);
+    const error = new Error();
+    if (!post) {
+      error.message = '게시글이 존재하지 않습니다.';
+      error.status = 404;
+      throw error;
+    } else if (!title || !content) {
+      error.message = '데이터 형식이 올바르지 않습니다.';
+      error.status = 400;
+      throw error;
+    } else if (user_id != post.User_id) {
+      error.message = '게시글 수정 권한이 존재하지 않습니다.';
+      error.status = 403;
+      throw error;
+    } else if (typeof title != 'string') {
+      error.message = '게시글 제목의 형식이 일치하지 않습니다.';
+      error.status = 400;
+      throw error;
+    } else if (typeof content != 'string') {
+      error.message = '게시글 내용의 형식이 일치하지 않습니다.';
+      error.status = 400;
+      throw error;
+    }
     await this.postRepository.updatePost(
       user_id,
       post_id,
       title,
       content,
-      likes
+      post.likes
     );
 
     const updatePost = await this.postRepository.findOnePost(post_id);
@@ -83,31 +125,73 @@ class PostService {
   };
 
   deletePost = async (post_id, user_id) => {
-    const findPost = await this.postRepository.findOnePost(post_id);
-    if (!findPost) throw new Error("Post doesn't exist");
-
+    const post = await this.postRepository.findOnePost(post_id);
+    const error = new Error();
+    if (!post) {
+      error.message = '게시글이 존재하지 않습니다.';
+      error.status = 404;
+      throw error;
+    } else if (user_id != post.User_id) {
+      error.message = '게시글 삭제 권한이 존재하지 않습니다.';
+      error.status = 403;
+      throw error;
+    }
     await this.postRepository.deletePost(post_id, user_id);
 
     return {
-      post_id: findPost.post_id,
-      User_id: findPost.User_id,
-      nickname: findPost.nickname,
-      title: findPost.title,
-      content: findPost.content,
-      createdAt: findPost.createdAt,
-      updatedAt: findPost.updatedAt,
+      post_id: post.post_id,
+      User_id: post.User_id,
+      nickname: post.nickname,
+      title: post.title,
+      content: post.content,
+      createdAt: post.createdAt,
+      updatedAt: post.updatedAt,
     };
   };
 
   createLike = async (post_id, user_id) => {
-    const findPost = await this.postRepository.findOnePost(post_id);
-    if (!findPost) throw new Error("Post doesn't exist");
+    const post = await this.postRepository.findOnePost(post_id);
+    const like = await this.postRepository.findOneLike(post_id, user_id);
+    const error = new Error();
 
-    const like = await this.postRepository.createLike(post_id, user_id);
+    let updateStatus = 0;
+    let message = '';
+    if (!post) {
+      error.message = '게시글이 존재하지 않습니다.';
+      error.status = 404;
+      throw error;
+    } else if (!like) {
+      await this.postRepository.createLike(post_id, user_id);
+      const post = await this.postRepository.findOnePost(post_id);
+      const findAllLike = await this.postRepository.findAllLike(post_id);
+
+      await this.postRepository.updatePost(
+        post.User_id,
+        post.post_id,
+        post.title,
+        post.content,
+        findAllLike.length
+      );
+
+      return {
+        updateStatus: 200,
+        message: '좋아요를 등록하였습니다.',
+      };
+    }
+    await this.postRepository.deleteLike(post_id, user_id);
+    const findAllLike = await this.postRepository.findAllLike(post_id);
+
+    await this.postRepository.updatePost(
+      post.User_id,
+      post.post_id,
+      post.title,
+      post.content,
+      findAllLike.length
+    );
 
     return {
-      Post_id: like.Post_id,
-      User_id: like.User_id,
+      updateStatus: 200,
+      message: '좋아요를 취소하였습니다.',
     };
   };
 
@@ -129,36 +213,6 @@ class PostService {
         updatedAt: post.updatedAt,
       };
     });
-  };
-
-  findAllLike = async (post_id) => {
-    const allLike = await this.postRepository.findAllLike(post_id);
-
-    return allLike;
-  };
-
-  findOneLike = async (post_id, user_id) => {
-    const like = await this.postRepository.findOneLike(post_id, user_id);
-
-    return {
-      Post_id: like.Post_id,
-      User_id: like.User_id,
-      createdAt: like.createdAt,
-      updatedAt: like.updatedAt,
-    };
-  };
-
-  deleteLike = async (post_id, user_id) => {
-    const findPost = await this.postRepository.findOnePost(post_id);
-    if (!findPost) throw new Error("Post doesn't exist");
-    await this.postRepository.deleteLike(post_id, user_id);
-
-    return {
-      Post_id: findPost.Post_id,
-      User_id: findPost.User_id,
-      createdAt: findPost.createdAt,
-      updatedAt: findPost.updatedAt,
-    };
   };
 }
 
